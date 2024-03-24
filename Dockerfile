f0c8a91b26495c4fa5a8abe7ad51ec8a65dd9a4c
clemoseitano <clemoseitano@outlook.com>
2024-03-24 11:31:23 +0000

    Updated tasks and docker files

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 # Pull base image
-FROM python:3.11-slim
+FROM python:3.12-slim
+
+# Update and install libmagic
+RUN apt-get update && \
+    apt-get install -y libmagic1 && \
+    rm -rf /var/lib/apt/lists/* \
 
 # Set environment variables
 ENV PIP_DISABLE_PIP_VERSION_CHECK 1
@@ -16,4 +21,12 @@ RUN pip install -r requirements.txt
 
 # Copy project
 COPY . .
+
+# Expose port 8000 to the outside world
 EXPOSE 8000
+
+# Copy entrypoint script and grant execution permissions
+COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+# Command to run the application using Daphne
+CMD ./entrypoint.sh
--- project_name/tasks.py
+++ project_name/tasks.py
@@ -1,12 +1,19 @@
 import json
+import logging
+import os
 import time
 
 from django.template.loader import render_to_string
+from django_celery_beat.models import PeriodicTask
 
 from celery import shared_task
 
+logger = logging.getLogger()
 
-@shared_task(email="", subject="", template_name="", template_fields_json_str="{}", message_id="")
+
+@shared_task(
+    email="", subject="", template_name="", template_fields_json_str="{}", message_id=""
+)
 def send_mails(email, subject, template_name, template_fields_json_str, message_id):
     """
     Send mails to users
@@ -19,18 +26,31 @@ def send_mails(email, subject, template_name, template_fields_json_str, message_
 
     """
     from django.core.mail import send_mail
-    from django.conf import settings
 
-    message = render_to_string(template_name, json.loads(template_fields_json_str))
-
-    is_sent = send_mail(
-        subject=subject,
-        message=message,
-        recipient_list=email.split(";"), fail_silently=False, from_email=None)
-    print(f"Email sent {is_sent}")
-
-    # Remove task for sent email
-    if is_sent:
+    logger.debug(f"Sending emails to {email}.")
+    try:
+        from_email = f"{os.environ.get('EMAIL_HOST_USER')}"
+        message = render_to_string(template_name, json.loads(template_fields_json_str))
+        is_sent = send_mail(
+            subject,
+            message,
+            from_email,
+            email.split(";"),
+            fail_silently=False,
+            html_message=message,
+        )
+        logger.debug(f"Email sent {is_sent}")
+        # Remove task for sent email
         from django_celery_beat.models import PeriodicTask
-        task = PeriodicTask.objects.filter(name=message_id)
-        task.delete()
+
+        task = PeriodicTask.objects.filter(name=message_id).first()
+        if is_sent != 0:
+            task.enabled = False
+        else:
+            total = task.total_run_count + 1
+            task.total_run_count = total
+            if total >= 3:
+                task.enabled = False
+        task.save()
+    except Exception as e:
+        logger.debug(e)
